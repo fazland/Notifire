8fd6b30ed1f13320104e56b07df9bba3e8c26bf1
Alessandro Chitolina <alessandro.chitolina@fazland.com>
2016-10-20 10:00:39 +0200

    renamed RandStrategy

--- lib/HandlerSelectorStrategy/RandStrategy.php
+++ lib/HandlerSelectorStrategy/RandStrategy.php
@@ -2,13 +2,13 @@
 
 namespace Fazland\Notifire\HandlerSelectorStrategy;
 
-class RandStrategySelectorStrategy implements HandlerSelectorStrategyInterface
+class RandStrategy implements HandlerSelectorStrategyInterface
 {
     /**
      * {@inheritdoc}
      */
     public function select($handlers)
     {
-        return $handlers[rand(0, count($handlers) - 1)];
+        return $handlers[mt_rand(0, count($handlers) - 1)];
     }
 }
